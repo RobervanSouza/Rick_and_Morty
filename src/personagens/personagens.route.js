d33c05a460274f9dfa3a67266d939e72f827587f
RobervanSouza <you@example.com>
2022-07-18 21:49:34 -0300

    ok

--- src/personagens/personagens.route.js
+++ src/personagens/personagens.route.js
@@ -6,35 +6,30 @@ const { validId, validObjectBody } = require('./personagens.middleware');
 
 route.get(
   '',
-  authMiddleware,
   controllerPersonagens.findAllPersonagensController,
 );
 route.get(
   '/find/:id',
-  authMiddleware,
   validId,
   controllerPersonagens.findByIdPersonagenController,
 );
 route.post(
   '/create',
-  authMiddleware,
   validObjectBody,
   controllerPersonagens.createPersonagenController,
 );
 route.put(
   '/update/:id',
-  authMiddleware,
   validId,
   validObjectBody,
   controllerPersonagens.updatePersonagenController,
 );
 route.delete(
   '/delete/:id',
-  authMiddleware,
   validId,
   controllerPersonagens.deletePersonagenController,
 );
 
-route.get('/search',authMiddleware, controllerPersonagens.pesquisaPersonagenController);
+route.get('/search', controllerPersonagens.pesquisaPersonagenController);
 
 module.exports = route;
